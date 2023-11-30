aa4b310384b10604b8ea3c9744e1be27c88c826e
KarishImrith <karish@imrithsolutions.com>
2023-11-30 20:08:05 +0200

    Added configuration extension method for easily getting app settings via the options pattern.

---
+++ demo-onlinestore-app/OnlineStore.App/Helpers/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineStore.App.Helpers;
+
+public static class Configuration
+{
+    public static TConfig GetConfig<TConfig>(this IConfiguration configuration)
+        where TConfig : class, new()
+        => configuration.GetSection(typeof(TConfig).Name).Get<TConfig>();
+}
